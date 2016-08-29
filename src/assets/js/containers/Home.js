2f26e135e2a1057a8c2db360adb143a133cc0079
Rich Cordero <rich@corderophilosophy.com>
2016-08-29 12:15:49 -0400

    Resize images, add captions

--- src/assets/js/containers/Home.js
+++ src/assets/js/containers/Home.js
@@ -3,6 +3,43 @@ const img1 = require('../../img/bcan1.jpg');
 const img2 = require('../../img/bcan2.jpg');
 const img3 = require('../../img/bcan3.jpg');
 // const img4 = require('./bcan4.jpg');
+//
+// Basic structure of comic object for S3 fetch/get
+// const comic = {
+//   id_ : "79047219792173-291",
+//   title: "",
+//   date: "",
+//   images: {
+//     img1: {
+//       src: 'source',
+//       caption: {
+//         Ned: "",
+//         Nimbus: "",
+//       }
+//     },
+//     img2: {
+//       src: 'source',
+//       caption: {
+//         Ned: "",
+//         Nimbus: "",
+//       }
+//     },
+//     img3: {
+//       src: 'source',
+//       caption: {
+//         Ned: "",
+//         Nimbus: "",
+//       }
+//     },
+//     img4: {
+//       src: 'source',
+//       caption: {
+//         Ned: "",
+//         Nimbus: "",
+//       }
+//     }
+//   }
+// };
 
 export default class Home extends Component {
   constructor(props) {
@@ -13,33 +50,37 @@ export default class Home extends Component {
     return (
       <div className="pt6 mw6 mw8-ns center pa3 ph5-ns">
         <article className="cf">
-          <div className="fl w-100 w-50-ns bg-near-white tc">
+          <div className="fl w-100 w-50-ns tc">
             <img
               className="mw-100"
               src={img1}
               alt="Nimbus and Ned - First Panel"
               title="Nimbus and Ned - First Panel"
             />
+            <p className="lh-solid fw7">Nimbus: NED! Something's happening!</p>
           </div>
-          <div className="fl w-100 w-50-ns bg-light-gray tc">
+          <div className="fl w-100 w-50-ns tc">
             <img
               className="mw-100"
               src={img2}
               alt="Nimbus and Ned - Second Panel"
               title="Nimbus and Ned - Second Panel"
             />
+            <p className="lh-solid fw7">Ned: What??</p>
           </div>
         </article>
         <article className="cf">
-          <div className="fl w-100 w-50-ns bg-near-white tc">
+          <div className="fl w-100 w-50-ns tc">
             <img
               className="mw-100"
               src={img3}
               alt="Nimbus and Ned - 3rd Panel"
               title="Nimbus and Ned - 3rd Panel"
             />
+            <p className="lh-solid fw7">Nimbus: We're a webcomic!</p>
+            <p className="lh-solid fw7">Ned: WHAT!?!?</p>
           </div>
-          <div className="fl w-100 w-50-ns bg-light-gray tc">
+          <div className="fl w-100 w-50-ns tc">
             <img
               className="mw-100"
               src={img1}
